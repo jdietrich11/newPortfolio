8951f6f8598b2f1900cba840eec7c64b3c406092
Joshua Dietrich <jbdietrich2@gmail.com>
2024-03-18 03:15:50 -0600

    added navigation

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -6,11 +6,17 @@ import "./header.styles.scss";
 const Header: React.FC = () => {
   return (
     <div className="headerContainer">
+      <Link to="/resume" className="linkContainerResume">
+        <div>Resume</div>
+      </Link>
       <Link to="/" className="nameContainer">
         <div className="name">Josh Dietrich</div>
         <div className="titleContainer">
           <div className="title">Web Developer</div>
         </div>
+      </Link>
+      <Link to="/about" className="linkContainerAbout">
+        <div>About</div>
       </Link>
     </div>
   );
