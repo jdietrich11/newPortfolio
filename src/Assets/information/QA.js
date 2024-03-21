5ccce326ce55f0d3f65fa2b6c8f667ffc079c8e0
Joshua Dietrich <jbdietrich2@gmail.com>
2024-03-21 03:44:14 -0600

    updated QA items

--- src/Assets/information/QA.js
+++ src/Assets/information/QA.js
@@ -159,9 +159,229 @@ export const moviesQA = [
     answers: [""],
   },
 ];
-export const showsQA = [{ id: 1, question: "", answer: "", answers: [""] }];
-export const carsQA = [{ id: 1, question: "", answer: "", answers: [""] }];
-export const miscQA = [{ id: 1, question: "", answer: "", answers: [""] }];
-export const outdoorsQA = [{ id: 1, question: "", answer: "", answers: [""] }];
-export const sportsQA = [{ id: 1, question: "", answer: "", answers: [""] }];
-export const familyQA = [{ id: 1, question: "", answer: "", answers: [""] }];
+export const showsQA = [
+  {
+    id: 1,
+    question: "What are some of your favorite TV-Shows",
+    answer: "",
+    answers: [
+      "How I Met Your Mother",
+      "Avatar The Last Airbender",
+      "Psych",
+      "Attack On Titan",
+      "Top Gear",
+      "Grand Tour",
+      "House M.D.",
+      "Parks and Rec",
+      "Big Bang Theory",
+      "Arcane",
+      "Suits",
+      "Devil as a Part-Timer",
+      "Star Wars: Rebels",
+      "Star Wars: Clone Wars",
+      "Sword Art Online",
+      "Frieren",
+    ],
+  },
+  {
+    id: 2,
+    question: "Why How I Met Your Mother",
+    answer:
+      "Admittedly not many people would say this was their favorite show. Many people were not happy with the ending. I am one who does however. To me this show is funny, relatable, and actually deals with problems real people deal with. I still remember the first time I saw the episode where Marshal's father died. Why not friends? to me the difference is that friends does't really relate to these kinds of problems. I would say if you're a fan of just plane 'happily ever after', then watch friends. If you enjoy real things, real life then watch HIMYM. I also have a-lot of respect for the writers having an ending in mind and sticking to it, even when it wasn't very popular.",
+    answers: [""],
+  },
+  {
+    id: 3,
+    question: "Why ATLA in 2nd",
+    answer:
+      "Weirdly enough I would actually say that Avatar is a more perfect and better tv show than HIMYM. I just like HIMYM slightly better. Avatar is a flawless show with extraordinary story telling, comedy, and showing relatable problems.",
+    answers: [""],
+  },
+  {
+    id: 4,
+    question: "Why such a long list",
+    answer:
+      "There are a lot of shows I like and some are hard to narrow down. For example I think the when The Office is funny. I think it's better than Parks and Rec, however I think Parks and Rec is more consistantly funnier. There's just so much I enjoy and tend to watch them in the background while working on a project so it's not quiet.",
+    answers: [""],
+  },
+  {
+    id: 5,
+    question: "What are your TV-Show hot takes",
+    answer:
+      "Breaking Bad, and Game of Thrones are not just over-rated but appaulingly terrible shows",
+    answers: [""],
+  },
+];
+export const carsQA = [
+  {
+    id: 1,
+    question: "What is your dream car",
+    answer:
+      "1969-1972 Chevrolet Chevelle is my dream project car. 2015 Ferrari 488 is my dream super car. 2013-2016 Audi S4 is my 'Realistic' dream car. 2016 Apollo Arrow is my most unrealistic dream car.",
+    answers: [""],
+  },
+  {
+    id: 2,
+    question: "What cars have you owned",
+    answer:
+      "my first car was a 2001 Audi S4 and is my favorite car i've owned. From there I briefly owned a 2015 Kia Optima. I traded that car in for a 2013 Camaro SS which I drove for awhile. Due to the bills that came along with school I had to give that up and now currently drive a 2012 Dodge Avenger SXT.",
+    answers: [""],
+  },
+  {
+    id: 3,
+    question: "What car would you like to get next",
+    answer:
+      "Likely, I'll get a 2013-2016 Audi S4. I'm also tempted to pick up a 2015 Ford Focus ST, A Corvette, or another Camaro. Due to gas prices I'm also tempted towards a Tesla or Cheverolet Volt. Unless I decide to just get a bike instead",
+    answers: [""],
+  },
+  {
+    id: 4,
+    question: "What about racing",
+    answer:
+      "Over the past few years I've started to lightly follow F1 racing. I really like the current rivalry between Lewis Hamilton and Max Verstappen. For the first time I actually watched an entire race, which was the 2021 final race in Abu Dhabi. I really enjoyed it and am counting down the days till the 2026 season. Though I'm not happy with how that particular race ended and hope the people in charge of F1 make some changes for this upcoming season.",
+    answers: [""],
+  },
+  {
+    id: 4,
+    question: "Do you have a favorite team",
+    answer:
+      "Yes, I root for Corvette racing in the GT3-AM racing, and Ferrari in F1",
+    answers: [""],
+  },
+];
+export const miscQA = [
+  {
+    id: 1,
+    question: "What do you mean by Misc. Projects",
+    answer:
+      "I like to build and create a lot of different things. Rather than have a small section for each or try to mix them in with other things I did a general section to fit them into.",
+    answers: [""],
+  },
+  {
+    id: 2,
+    question: "Virtual Pinball Cabinet",
+    answer:
+      "I was introduced to one of these from my Highschool girlfriends dad and decided to try to build one myself. loaded up with over 200 different virtual pinball tables. Using programs like Visual Pinball and PinballFX. Wired up with light up buttons, strobes, cabinet lights, sirens, and a fan. This is one of my most complete project's I've worked on. Currently I'm rebuilding it in order to do some wire managment and finish up some other addons with some more to come like a bell and a knocker.",
+    answers: [""],
+  },
+  {
+    id: 3,
+    question: "Game Ogre",
+    answer:
+      "This is a Virtual Arcade cabinet. This contains over 3000 games and runs everthing, I know of, Nintendo 64 and older. Currently it only works with joystick and button, or controller games some aiming guns for arcade shooters are currently being developed that will work with the modern screens I'm hoping to add that capability to this project. It also still needs to be painted and add a few other finishing touches.",
+    answers: [""],
+  },
+  {
+    id: 4,
+    question: "Desk",
+    answer:
+      "I built this desk to take with me to my apartment when I lived in provo. I wanted the open shelves so I could place a projector on it so I could work at my desk comfortably and still have a TV-Show on in the background.",
+    answers: [""],
+  },
+  {
+    id: 5,
+    question: "Table",
+    answer:
+      "Made out of red-wood this is probably one of the coolest things I've made. Custom made to hold my TV and a shelf to keep my gaming consoles off the carpet.",
+    answers: [""],
+  },
+  {
+    id: 6,
+    question: "Wood Engraving",
+    answer:
+      "Starting out by hand I enjoy doing some pyrography. I like the smell of burning wood and how it looks. More Recently I've invested in a Laser engraver and have created family plaques for my mom and siblings, wooden Christmas ornaments, and some other things.",
+    answers: [""],
+  },
+  {
+    id: 7,
+    question: "UE5",
+    answer:
+      "This is my unnamed UE5 project idea, not yet started. I'm working on learning UE5 and then starting this project",
+    answers: [""],
+  },
+];
+export const outdoorsQA = [
+  {
+    id: 1,
+    question: "What Outdoor activities do you enjoy",
+    answer: "",
+    answers: [
+      "Camping",
+      "Motorcycle riding (dirt and street)",
+      "Road Trips",
+      "Canyon Drives",
+      "ATVs",
+      "Rock Climbing",
+    ],
+  },
+  {
+    id: 2,
+    question: "Do you have a favorite camping spot",
+    answer:
+      "yeah, growing up my family would go on a yearly camping trip over the summer with my extended family. We would usually go to a place called Potters Ponds. We would go on 4-Wheeler rides, my favorite is trail #5. We would also have Airsoft wars in the forested areas, and always do a friday night movie, using a projector we would watch a movie while it was dark by the fire.",
+    answers: [""],
+  },
+];
+export const sportsQA = [
+  {
+    id: 1,
+    question: "What Sports",
+    answer: "",
+    answers: [
+      "Basketball (Go Jazz)",
+      "Football (Go Saints)",
+      "Golf",
+      "Go Karting",
+    ],
+  },
+  {
+    id: 2,
+    question: "Did you ever play any sports competitively",
+    answer:
+      "Aside from intramural Basketball not in college. In High School I was a QB on the football team until I tore my ACL. I tried out for the Basktball team but did not make it? Until recently, my Dad and I would enter the Bluffdale City golf tournament each year.",
+    answers: [""],
+  },
+  {
+    id: 3,
+    question: "Do you play any other Sports",
+    answer:
+      "I don't get alot of opportunites to play many other sports but I can appreciate most sports and I'm always up for it.",
+    answers: [""],
+  },
+];
+export const familyQA = [
+  {
+    id: 1,
+    question: "How big is your family",
+    answer:
+      "Which one, I come from a biological family of 9 falling at number 8. I also have a non-biological family as I believe, close friends are just the family you get to choose",
+    answers: [""],
+  },
+  {
+    id: 2,
+    question: "How big is your non-bio family",
+    answer: "",
+    answers: [
+      "Brandon and Elenore Chambers - Brandon was my roomate for 1 semester at snow college and married one of the greatest women in the entire world and we've grown close",
+      "Lauren Smith - A friend of Elenores who invited me to join their DND group and we've grown close",
+      "Konlin and Cydnee Gappmayre - Konlin and I worked together at LHM Ford we became roomates for a year and is still one of my closest brothers",
+      "Andrew Aagard - one of my oldest and most loyal friends since I was 12",
+      "Steven Skoubye - My oldest and most loyal friend, since we were 5 there is nobody I look up to more in life",
+      "Zackary McClellan - My 2nd oldest friend Steven, Zack, and I were a trio all growing up in our neighborhood. Zack is one of the realest hardest worker I've ever met",
+    ],
+  },
+  {
+    id: 3,
+    question: "What makes them Family, not just Friends",
+    answer:
+      "I would give each of these people an organ with no explanation necessary when asked. Each year we go on a camping trip to Potters Ponds, these are also the people I have grown with the most and I rely on the most",
+    answers: [""],
+  },
+  {
+    id: 4,
+    question: "What about your bio family",
+    answer:
+      "They're ok, I honestly like the in-laws a little more than the actual siblings though ;)",
+    answers: [""],
+  },
+];
